0813d2bef6be31a1338017252a90413cd958d5b7
Alexander <alexander.dyplin@utb.ecutbildning.se>
2024-03-01 10:56:16 +0100

    Add project files.

---
+++ WebApp/Models/SignUpModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Helpers;
+
+namespace WebApp.Models;
+
+public class SignUpModel
+{
+    [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
+    public string FirstName { get; set; } = null!;
+
+
+    [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null!;
+
+
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage ="Invalid email address")]
+    [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Please enter a valid e-mail adress")]
+    public string Email { get; set; } = null!;
+
+
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Invalid password")]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Please enter a valid password.")]
+
+    public string Password { get; set; } = null!;
+
+
+    [Display(Name = "Confirm password", Prompt = "Confirm your password", Order = 4)]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password must be confirmed")]
+    [Compare(nameof(Password), ErrorMessage ="Passwords does not match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Display(Name = "I agree to the Terms & Conditions", Order = 5)]
+    [CheckBoxRequired(ErrorMessage ="You must accept the terms and conditions to proceed.")]
+    public bool TermsAndConditions { get; set; } = false;
+}
+
+
+
+
+
