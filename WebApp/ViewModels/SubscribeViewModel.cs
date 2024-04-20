92d6061c5c92eb686c7aa6579cda849ef4595a95
Alexander <alexander.dyplin@utb.ecutbildning.se>
2024-04-20 11:41:35 +0200

    small fixes to the newsletter

--- WebApp/ViewModels/SubscribeViewModel.cs
+++ WebApp/ViewModels/SubscribeViewModel.cs
@@ -7,7 +7,6 @@ public class SubscribeViewModel
     [Required]
     [EmailAddress]
     [Display(Name = "E-mail address", Prompt ="Your Email")]
-    [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = null!;
     public bool DailyNewsletter { get; set; }
     public bool AdvertisingUpdates { get; set; }
